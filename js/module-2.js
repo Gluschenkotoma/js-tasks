73c220a2ff67129347c5b0084d7bff405bbeca67
Tamara Glushchenko <gluschenkotoma@ukr.net>
2021-06-23 17:04:26 +0300

    7

--- js/module-2.js
+++ js/module-2.js
@@ -417,3 +417,394 @@
 // console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
 
 // task17
+// Дополни цикл for так, чтобы он логировал все целые числа в диапазоне от start до end включительно.
+// Тесты
+// Объявлена переменная start
+// Значение переменной start это число 3
+// Объявлена переменная end
+// Значение переменной end это число 7
+// Объявлена переменная i - счётчик цикла
+// Начальное значение переменной i равно 3
+// Условие цикла приводится к true до тех пор, пока i меньше либо равно 7
+// На каждой итерации значение переменной i увеличивается на единицу
+// Вывод в консоль переменной i покажет числа 3, 4, 5, 6, 7
+
+// const start = 3;
+// const end = 7;
+
+// for (let i = start; i <= end; i += 1) {
+//   // Change this line
+//   console.log(i);
+// }
+
+// task18
+// Задача: сумма чисел (цикл for)
+// Задание
+// Напиши функцию calculateTotal(number), которая принимает целое число(параметр number) и возвращает сумму всех целых чисел от единицы и до этого числа.
+// Например, если number равно 3, то сумма это 1 + 2 + 3, то есть 6.
+// Тесты
+// Объявлена функция calculateTotal(number)
+// Вызов функции calculateTotal(1) возвращает 1
+// Вызов функции calculateTotal(3) возвращает 6
+// Вызов функции calculateTotal(7) возвращает 28
+// Вызов функции calculateTotal(18) возвращает 171
+// Вызов функции calculateTotal(24) возвращает 300
+// Вызов функции calculateTotal() со случайным числом возвращает правильное значение
+
+// function calculateTotal(number) {
+//   let result = 0;
+//   for (let i = 0; i <= number; i += 1) {
+//     result += i;
+//   }
+//   return result;
+// }
+// console.log(calculateTotal(1));
+// console.log(calculateTotal(3));
+// console.log(calculateTotal(7));
+// console.log(calculateTotal(18));
+// console.log(calculateTotal(24));
+
+// task19
+// Итерация по массиву
+// Цикл for можно использовать для итерации по массиву, то есть «перебрать» его поэлементно.
+// const planets = ["Earth", "Mars", "Venus"];
+// for (let i = 0; i < planets.length; i += 1) {
+//   console.log(planets[i]);
+// }
+// Для доступа к элементам используется синтаксис квадратных скобок массив[индекс],
+// где индекс - это значение счётчика цикла от 0 и до последнего индекса массива, который на единицу меньше длины массива.
+
+// Задание
+// Дополни код цикла for так, чтобы он последовательно логировал все элементы массива fruits.
+// Тесты
+// Объявлена переменная fruits
+// Значение переменной fruits это массив ["apple", "plum", "pear", "orange"]
+// Объявлена переменная i - счётчик цикла
+// Начальное значение переменной i равно 0
+// Условие цикла приводится к true до тех пор, пока i меньше 4
+// На каждой итерации значение переменной i увеличивается на единицу
+// В теле цикла for объявляется переменная const fruit и этой переменной присваивается значение - элемент массива
+// В теле цикла for используется вывод в консоль переменной fruit
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// for (let i = 0; i < fruits.length; i += 1) {
+//   const fruit = fruits[i];
+//   console.log(fruit);
+// }
+
+//   task20
+// Задача: подсчёт суммы покупки
+// Задание
+// Напиши функцию calculateTotalPrice(order), которая принимает один параметр order - массив чисел,
+// и рассчитывает общую сумму его элементов.Общая сумма элементов должна сохраняться в переменной total, которая возвращается, как результат работы функции.
+// Тесты
+// Объявлена функция calculateTotalPrice(order)
+// Вызов функции calculateTotalPrice([12, 85, 37, 4]) возвращает 138
+// Вызов функции calculateTotalPrice([164, 48, 291]) возвращает 503
+// Вызов функции calculateTotalPrice([412, 371, 94, 63, 176]) возвращает 1116
+// Вызов функции calculateTotalPrice() со случайным массивом возвращает правильное значение
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   for (let i = 0; i < order.length; i += 1) {
+//     total += order[i];
+//   }
+//   return total;
+// }
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+// console.log(calculateTotalPrice([164, 48, 291]));
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
+// task21
+// Задача: поиск самого длинного слова
+// Задание
+// Напиши функцию findLongestWord(string) которая принимает произвольную строку состоящую только из слов разделённых пробелом(параметр string)
+// и возвращает самое длинное слово в этой строке.
+// Тесты
+// Объявлена функция findLongestWord(string)
+// Вызов функции findLongestWord("The quick brown fox jumped over the lazy dog") возвращает jumped
+// Вызов функции findLongestWord("Google do a roll") возвращает Google
+// Вызов функции findLongestWord("May the force be with you") возвращает force
+// Вызов функции findLongestWord() со случайной строкой возвращает правильное значение
+
+// function findLongestWord(string) {
+//   let longestWord = "";
+//   const array = string.split(" ");
+
+//   for (let i = 0; i < array.length; i += 1) {
+//     if (longestWord.length < array[i].length) {
+//       longestWord = array[i];
+//     }
+//   }
+//   return longestWord;
+// }
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+// console.log(findLongestWord("Google do a roll"));
+// console.log(findLongestWord("May the force be with you"));
+
+// task22
+// Задание
+// Дополни код функции createArrayOfNumbers(min, max) так,
+// чтобы она возвращала массив всех целых чисел от значения min до max.
+// Тесты
+// Объявлена функция createArrayOfNumbers(min, max)
+// Вызов функции createArrayOfNumbers(1, 3) возвращает [1, 2, 3]
+// Вызов функции createArrayOfNumbers(14, 17) возвращает [14, 15, 16, 17]
+// Вызов функции createArrayOfNumbers(29, 34) возвращает [29, 30, 31, 32, 33, 34]
+// Вызов функции createArrayOfNumbers() со случайными min и max возвращает правильный массив
+// В цикле for использовался метод push
+
+// function createArrayOfNumbers(min, max) {
+//   const numbers = [];
+//   for (let i = min; i <= max; i += 1) {
+//     numerbs.push(i);
+//   }
+//   return numbers;
+// }
+// console.log(createArrayOfNumbers(1, 3));
+// console.log(createArrayOfNumbers(14, 17));
+// console.log(createArrayOfNumbers(29, 34));
+
+// task23
+// Задача: фильтрация массива чисел
+// Задание
+// Напиши функцию filterArray(numbers, value), которая принимает массив чисел(параметр numbers) и возвращает новый массив,
+// в котором будут только те элементы массива numbers, которые больше чем значение параметра value(число).
+// Тесты
+// Объявлена функция filterArray(numbers, value)
+// Вызов функции filterArray([1, 2, 3, 4, 5], 3) возвращает [4, 5]
+// Вызов функции filterArray([1, 2, 3, 4, 5], 4) возвращает [5]
+// Вызов функции filterArray([1, 2, 3, 4, 5], 5) возвращает []
+// Вызов функции filterArray([12, 24, 8, 41, 76], 38) возвращает [41, 76]
+// Вызов функции filterArray([12, 24, 8, 41, 76], 20) возвращает [24, 41, 76]
+// Вызов функции filterArray() со случайным массивом и числом возвращает правильный массив
+// В цикле for использовался метод push
+
+// function filterArray(numbers, value) {
+//   const newArray = [];
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] > value) {
+//       newArray.push(numbers[i]);
+//     }
+//   }
+//   return newArray;
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+// task24
+// Задание
+// Функция checkFruit(fruit) принимает строку с названием фрукта (параметр fruit), и проверяет есть ли такой фрукт в массиве fruits.
+// Дополни код функции так, что если:
+// фрукт есть в массиве, то функция возвращает true;
+// фрукта нет в массиве, то функция возвращает false.
+// Тесты
+// Объявлена функция checkFruit(fruit)
+// Вызов checkFruit("plum") возвращает true
+// Вызов checkFruit("mandarin") возвращает false
+// Вызов checkFruit("pear") возвращает true
+// Вызов checkFruit("Pear") возвращает false
+// Вызов checkFruit("apple") возвращает true
+// Вызов функции checkFruit() со случайным словом возвращает правильное значение boolean
+// В функции использовался метод includes
+
+// function checkFruit(fruit) {
+//   const fruits = ["apple", "plum", "pear", "orange"];
+
+//   return fruits.includes(fruit); // Change this line
+// }
+// console.log(checkFruit("plum"));
+// console.log(checkFruit("mandarin"));
+// console.log(checkFruit("pear"));
+// console.log(checkFruit("Pear"));
+// console.log(checkFruit("apple"));
+
+// task25
+// Задача: общие элементы
+// Задание
+// Общими элементами массивов называют те элементы, которые присутствуют во всех массивах.
+//  Например, в двух массивах[1, 3, 5] и[0, 8, 5, 3] общими будут числа 3 и 5, т.к.они присутствуют в обоих исходных массивах.
+// А числа 0, 1 и 8 присутствуют только в одном из массивов.
+// Напиши функцию getCommonElements(array1, array2) которая получает два массива произвольной длины в параметры array1 и array2,
+// и возвращает новый массив, состоящий из тех элементов, которые присутствуют в обоих исходных массивах.
+// Тесты
+// Объявлена функция getCommonElements(array1, array2)
+// Вызов getCommonElements([1, 2, 3], [2, 4]) возвращает [2]
+// Вызов getCommonElements([1, 2, 3], [2, 1, 17, 19]) возвращает [1, 2]
+// Вызов getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]) возвращает [12, 27, 3]
+// Вызов getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]) возвращает [10, 30, 40]
+// Вызов getCommonElements([1, 2, 3], [10, 20, 30]) возвращает []
+// Вызов функции getCommonElements() со случайными двумя массивами возвращает правильный массив
+// В цикле for использовались методы includes и push
+
+// function getCommonElements(array1, array2) {
+//   const newArray = [];
+
+//   for (let i = 0; i < array1.length; i += 1) {
+//     if (array1.includes(array2[i])) {
+//       newArray.push(array1[i]);
+//     }
+//   }
+//   return newArray;
+// }
+// console.log(getCommonElements([1, 2, 3], [2, 4]));
+// console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+// console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+// console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
+
+// task26
+// Выполни рефакторинг кода функции calculateTotalPrice(order) заменив цикл for на for...of.
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
+
+//   for (let i = 0; i < order.length; i += 1) {
+//     total += order[i];
+//   }
+
+//   // Change code above this line
+//   return total;
+// }
+
+// Объявлена функция calculateTotalPrice(order)
+// Вызов функции calculateTotalPrice([12, 85, 37, 4]) возвращает 138
+// Вызов функции calculateTotalPrice([164, 48, 291]) возвращает 503
+// Вызов функции calculateTotalPrice([412, 371, 94, 63, 176]) возвращает 1116
+// Вызов функции calculateTotalPrice([]) возвращает 0
+// Вызов функции calculateTotalPrice() со случайным массивом чисел возвращает правильную сумму
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+
+//   for (const point of order) {
+//     total += point;
+//   }
+//     return total;
+// }
+// console.log(calculateTotalPrice([12, 85, 37, 4]));
+// console.log(calculateTotalPrice([164, 48, 291]));
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+// console.log(calculateTotalPrice([]));
+
+// task27
+// Зачада: фильтрация массива чисел 2.0
+// function filterArray(numbers, value) {
+
+//   const filteredNumbers = [];
+
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     const number = numbers[i];
+
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }
+
+//   return filteredNumbers;
+// }
+// Задание
+// Выполни рефакторинг функции filterArray(numbers, value) заменив цикл for на for...of.
+// Тесты
+// Объявлена функция filterArray(numbers, value)
+// Вызов функции filterArray([1, 2, 3, 4, 5], 3) возвращает [4, 5]
+// Вызов функции filterArray([1, 2, 3, 4, 5], 4) возвращает [5]
+// Вызов функции filterArray([1, 2, 3, 4, 5], 5) возвращает []
+// Вызов функции filterArray([12, 24, 8, 41, 76], 38) возвращает [41, 76]
+// Вызов функции filterArray([12, 24, 8, 41, 76], 20) возвращает [24, 41, 76]
+// Вызов функции filterArray() со случайным массивом и числом возвращает правильный массив
+// Функция calculateTotalPrice() использует цикл for..of
+
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+
+//   for (const number of numbers) {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }
+
+//   return filteredNumbers;
+// }
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+// console.log(filterArray([1, 2, 3, 4, 5], 4));
+// console.log(filterArray([1, 2, 3, 4, 5], 5));
+// console.log(filterArray([12, 24, 8, 41, 76], 38));
+// console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+// task28
+// Оператор %
+// Вместо того, чтобы возвращать результат деления, операция по модулю (%) возвращает целочисленный остаток от деления двух чисел - делимого и делителя.
+
+// 5 % 1 = 0;
+// // 5, разделенное на 1, равно 5, а остаток - 0
+
+// 5 % 2 = 1;
+// //  5, разделенное на 2, равно 2, а остаток - 1
+
+// 5 % 3 = 2;
+// //  5, разделенное на 3, равно 1, а остаток - 2
+
+// 5 % 4 = 1;
+// //  5, разделенное на 4, равно 1, а остаток - 1
+
+// 5 % 5 = 0;
+// //  5, разделенное на 5, равно 1, а остаток -
+
+// Задание
+// Дополни выражения остатка от деления так, чтобы код проходил тесты.
+// Тесты
+// Объявлена переменная a
+// Значение переменной a это число 0
+// Объявлена переменная b
+// Значение переменной b это число 1
+// Объявлена переменная c
+// Значение переменной c это число 3
+// Объявлена переменная d
+// Значение переменной d это число 5
+// Объявлена переменная e
+// Значение перемен e это число
+
+// const a = 3 % 1;
+// const b = 4 % 3;
+// const c = 11 % 4;
+// const d = 12 % 7;
+// const e = 8 % 3;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+// task29
+// Задача: чётные числа
+// Задание
+// Напиши функцию getEvenNumbers(start, end) которая возвращает массив всех чётных чисел от start до end.
+// Чётным считается число которое делится на 2 без остатка(10 % 2 === 0).
+// Тесты
+// Объявлена функция getEvenNumbers(start, end)
+// Вызов функции getEvenNumbers(2, 5) возвращает [2, 4]
+// Вызов функции getEvenNumbers(3, 11) возвращает [4, 6, 8, 10]
+// Вызов функции getEvenNumbers(6, 12) возвращает [6, 8, 10, 12]
+// Вызов функции getEvenNumbers(8, 8) возвращает [8]
+// Вызов функции getEvenNumbers(7, 7) возвращает []
+// Вызов функции getEvenNumbers() со случайными start и end возвращает правильный
+
+// function getEvenNumbers(start, end) {
+//   const arr = [];
+//   for (let i = start; i <= end; i += 1) {
+//     if (i % 2 === 0) {
+//       arr.push(i);
+//     }
+//   }
+
+//   return arr;
+// }
+// console.log(getEvenNumbers(2, 5));
+// console.log(getEvenNumbers(3, 11));
+// console.log(getEvenNumbers(6, 12));
+// console.log(getEvenNumbers(8, 8));
+// console.log(getEvenNumbers(7, 7));
