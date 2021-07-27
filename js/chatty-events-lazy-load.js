a92dbc5618a2644e913fcb9bef1ddf32fc0cb081
Tamara Glushchenko <gluschenkotoma@ukr.net>
2021-07-27 14:18:24 +0300

    32

--- js/chatty-events-lazy-load.js
+++ js/chatty-events-lazy-load.js
@@ -83,14 +83,74 @@
 // function populateList(markup) {
 //   refs.list.innerHTML = markup;
 // }
+
 // lazy-load============================================
-const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+// feature-detection
+// if ("loading" in HTMLImageElement.prototype) {
+//   console.log("Браузер поддерживает ленивки");
+//   const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+//   lazyImages.forEach((img) => {
+//     img.src = img.dataset.src;
+//   });
+// } else {
+//   console.log("Браузер не поддерживает ленивки");
+//   const script = document.createElement("script");
+//   script.src =
+//     "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+//   script.integrity =
+//     "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+//   script.crossorigin = "anonymous";
+//   script.referrerpolicy = "no-referrer";
+//   document.body.appendChild(script);
+// }
+
+// const lazyImages = document.querySelectorAll("img[data-src]");
+
+// lazyImages.forEach((image) => {
+//   //загрузка картинки
+//   image.addEventListener("load", onImageLoaded, { once: true });
+// });
+
+// function onImageLoaded(e) {
+//   //загрузка анимации
+//   console.log("Loaded");
+//   e.target.classList.add("appear");
+// }
+
+// Скоротить
+if ("loading" in HTMLImageElement.prototype) {
+  addSrcAttrToLazyImages();
+} else {
+  addLazySizesScript();
+}
+
+const lazyImages = document.querySelectorAll("img[data-src]");
 
 lazyImages.forEach((image) => {
+  //загрузка картинки
   image.addEventListener("load", onImageLoaded, { once: true });
 });
 
 function onImageLoaded(e) {
+  //загрузка анимации
   console.log("Loaded");
   e.target.classList.add("appear");
+}
+
+function addLazySizesScript() {
+  const script = document.createElement("script");
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+  script.integrity =
+    "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+  script.crossorigin = "anonymous";
+  script.referrerpolicy = "no-referrer";
+  document.body.appendChild(script);
+}
+
+function addSrcAttrToLazyImages() {
+  const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+  lazyImages.forEach((img) => {
+    img.src = img.dataset.src;
+  });
 }
